5912343eaae01368bd5d9e5a9e8775813c2268af
marcos <206247214+MarcosGitCode@users.noreply.github.com>
2026-04-16 10:29:59 -0300

    Feat: Logica botão entrar

---
+++ Assets/Telas/jogar ou estudar/TelaJogarManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TelaJogarManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Estudar(){
+        SceneManager.LoadScene("estudar");
+    }
+}

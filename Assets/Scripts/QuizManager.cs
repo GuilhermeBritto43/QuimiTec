52c7a61bfc936f5751f4212effd3981e9b49a642
Guilherme Britto <guibrittotava@gmail.com>
2026-04-29 15:55:41 -0300

    feat: desenvolve logica quizmanager

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -1,16 +1,112 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Mono.Data.Sqlite;
+using System.Data;
+using System.IO;
+using System.Collections.Generic;
 
 public class QuizManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TMP_Text textoPergunta;
+
+    public Button[] botoes; 
+
+    private string caminhoDB;
+
     void Start()
     {
-        
+        string nomeBanco = "QuimiTec.db";
+        string caminhoOrigem = Path.Combine(Application.streamingAssetsPath, nomeBanco);
+        string caminhoDestino = Path.Combine(Application.persistentDataPath, nomeBanco);
+
+        File.Copy(caminhoOrigem, caminhoDestino, true);
+
+        caminhoDB = "URI=file:" + caminhoDestino;
+
+        CarregarPergunta("facil");
     }
 
-    // Update is called once per frame
-    void Update()
+    void CarregarPergunta(string dificuldade)
     {
-        
+        using (var conexao = new SqliteConnection(caminhoDB))
+        {
+            conexao.Open();
+
+            int idPergunta = -1;
+            string texto = "";
+
+            using (var comando = conexao.CreateCommand())
+            {
+                comando.CommandText = @"
+                SELECT * FROM Perguntas
+                WHERE dificuldade = @dif
+                ORDER BY RANDOM()
+                LIMIT 1;";
+
+                comando.Parameters.Add(new SqliteParameter("@dif", dificuldade));
+
+                using (IDataReader leitor = comando.ExecuteReader())
+                {
+                    if (leitor.Read())
+                    {
+                        idPergunta = int.Parse(leitor["id"].ToString());
+                        texto = leitor["texto"].ToString();
+                    }
+                }
+            }
+
+            textoPergunta.text = texto;
+
+            List<(string img, bool correta)> alternativas = new List<(string, bool)>();
+
+            using (var comando = conexao.CreateCommand())
+            {
+                comando.CommandText = @"
+                SELECT * FROM Alternativas
+                WHERE pergunta_id = @id
+                ORDER BY RANDOM();";
+
+                comando.Parameters.Add(new SqliteParameter("@id", idPergunta));
+
+                using (IDataReader leitor = comando.ExecuteReader())
+                {
+                    while (leitor.Read())
+                    {
+                        string img = leitor["imagem_path"].ToString();
+                        bool correta = int.Parse(leitor["correta"].ToString()) == 1;
+
+                        alternativas.Add((img, correta));
+                    }
+                }
+            }
+
+            for (int i = 0; i < botoes.Length; i++)
+            {
+                var alt = alternativas[i];
+
+                Sprite sprite = Resources.Load<Sprite>(alt.img);
+                botoes[i].image.sprite = sprite;
+
+                bool resposta = alt.correta;
+
+                botoes[i].onClick.RemoveAllListeners();
+                botoes[i].onClick.AddListener(() => VerificarResposta(resposta));
+            }
+        }
+    }
+
+    void VerificarResposta(bool correta)
+    {
+        if (correta)
+        {
+            Debug.Log("ACERTOU!");
+        }
+        else
+        {
+            Debug.Log("ERROU!");
+        }
+
+        CarregarPergunta("facil");
     }
 }

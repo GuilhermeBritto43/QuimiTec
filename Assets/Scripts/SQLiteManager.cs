6329519c2522b617b7b2fdd57f5ce8d02e27a591
giu <giupassianimaua@gmail.com>
2026-05-19 10:47:59 -0300

    popup funcionando

--- Assets/Scripts/SQLiteManager.cs
+++ Assets/Scripts/SQLiteManager.cs
@@ -9,6 +9,8 @@ public class SQLiteManager : MonoBehaviour
 {
     private string caminhoDB;
     public TMP_Text textoListaAlunos;
+    public GameObject Popup;
+
 
     void Start()
     {
@@ -162,14 +164,21 @@ public class SQLiteManager : MonoBehaviour
                         else
                         {
                             Debug.Log("Domínio desconhecido");
+                            MostrarPopup();
                         }
                     }
                     else
                     {
                         Debug.Log("Email ou senha inválidos");
+                        MostrarPopup();
                     }
                 }
             }
         }
     }
+    public void MostrarPopup()
+    {
+        Popup.SetActive(true);
+    }
+    
 }
